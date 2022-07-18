f8720011d01829c30fc75f3b74574cab6c4b9a3f
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-18 13:18:53 -0600

    Feat: add password to user and test the user model for this functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_many :user_viewing_parties
   has_many :viewing_parties, through: :user_viewing_parties
+  has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,10 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password_digest }
   end
+
+  it { should have_secure_password }
 
   describe 'class methods' do
     describe '#other_users' do
